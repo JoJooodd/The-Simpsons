0d2a8604035cb79e5a89d04ba110c4d5f396f8ff
JoJooodd <124522958+JoJooodd@users.noreply.github.com>
2023-02-14 09:54:19 -0600

    Cambios en el gitignore
    Ignore modulos y sus archivos de la carpeta

--- main.py/main.py
+++ main.py/main.py
@@ -1,2 +1,10 @@
-print('Josue David Bautista Orózco, 1532523')
+import os
 
+mi_ubicacion = os.getcwd()
+if os.path.exists("modulos"):
+    print("La carpeta ya existe")
+else:
+    os.mkdir(mi_ubicacion + "\\modulos")
+    archivo = open('./modulos/prueba.txt', 'w')
+    archivo.write('Hola mundo')
+    archivo.close()
